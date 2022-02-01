e7653d4511e10c33f9803ccc833795da5d3adbf2
RaphaelEsparta <83084343+RaphaelEsparta@users.noreply.github.com>
2022-02-01 09:55:37 -0300

    no message

---
+++ src/layout/account/Login.js
@@ -0,0 +1,29 @@
+import './Login.css';
+
+function Login() {
+    return (
+      <div className="wrapper">
+    <div id="formContent">
+      <h2 className="active" > Sign In </h2>
+      <h2 className="inactive underlineHover" >Sign Up </h2>
+      <div className="In">
+        <img src="../../../assets/img/icon.png" id="icon" alt="User Icon" />
+      </div>
+      <form>
+        <input type="text" id="username" className="In" name="username" placeholder="login"  required>
+        </input>
+        <input type="text" id="password" className="In" name="password" placeholder="password" required>
+        </input>
+        <input type="submit" className="In" value="Log In" >
+        </input>
+      </form>
+      <div id="formFooter">
+        <a className= "underlineHover" href="#">Esqueceu a senha?</a>
+      </div>
+    </div>
+  </div>
+    );
+  }
+  
+  export default Login;
+  
---
+++ src/layout/navbar/nabvar.js
@@ -0,0 +1,30 @@
+import './Login.css';
+
+function NavBar() {
+  return (
+    <div className="main-bar">
+        <nav>
+            <input type="checkbox" id="check"></input>
+            <label htmlFor="check" class="checkbtn">
+                <span></span>
+                <span></span>
+                <span></span>
+            </label>
+            <div className="logo-header">
+                <a href="#">RaviWeb</a>
+            </div>
+            <ul>
+                <li className="active"><a href="#">Home</a></li>
+                <li><a href="#">About Us</a></li>
+                <li><a href="#">Our Services</a></li>
+                <li><a href="#">Our Gallery</a></li>
+                <li><a href="#">Enquiry</a></li>
+                <li><a href="#">Contact Us</a></li>
+            </ul>
+        </nav>
+    </div>
+
+  );
+}
+
+export default NavBar;
